340fca0df57aa9828f5b5ea1af7afdb65bd61441
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-05-03 10:46:13 -0500

    solved problem6

--- js/app.js
+++ js/app.js
@@ -90,7 +90,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+//testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -133,18 +133,24 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
-function multiplyAnyArray(dynamicArray) {
-  //eslint-disable-line
-  let arr1 = [2, 3, 4];
-  let total = 0;
-  for (let i= 0; i < arr1.length; i++) {
-    total += sumArray[i];
+function multiplyAnyArray(dynamicArray) {//eslint-disable-line
+  let total = 1;
 
+  for ( let i = 0; i < dynamicArray.length; i++) {
+    total = total * dynamicArray[0];
+    total = total * dynamicArray[1];
+    total = total * dynamicArray[2];
+    total = total * dynamicArray[3];
+    total = total * dynamicArray[4];
+    let string = `The numbers ${dynamicArray[0]},${dynamicArray[1]},${dynamicArray[2]},${dynamicArray[3]},${dynamicArray[4]} have a product of ${total}.`;
+    return [total,string];
 
   }
-}
 
+
+}
+console.log(multiplyAnyArray([1, 2, 3, 4, 5]));
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
